e1e9a3b889951cf608f1eaea2874a206ea216d4a
SolublePotash <162484667+SolublePotash@users.noreply.github.com>
2024-09-05 18:50:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(cors());
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://discord.com';
+const nggUrl = 'https://mathsspot.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -15,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'discord.com') {
+    if (req.headers.host === 'mathsspot.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
